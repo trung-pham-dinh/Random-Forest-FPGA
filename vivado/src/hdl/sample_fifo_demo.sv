11aca9d2d0044ff07294e8bd724c7782d85ee695
Nguyễn Phan Anh Tuấn <79034091+KiuPhox@users.noreply.github.com>
2023-03-12 16:49:15 +0700

    Update sample_fifo_demo.sv

--- vivado/src/hdl/sample_fifo_demo.sv
+++ vivado/src/hdl/sample_fifo_demo.sv
@@ -2,7 +2,7 @@
 
 module sample_fifo #(
      parameter FIFO_WIDTH = 16
-    ,parameter FIFO_DEPTH_BIT = 4 // DEPTH_BIT**2 labels
+    ,parameter FIFO_DEPTH_BIT = 4 // number of label for classification problem
 ) (
      input                              clk
     ,input                              rst_n
@@ -14,40 +14,64 @@ module sample_fifo #(
     ,input                              i_push
     ,input [FIFO_WIDTH-1:0]             i_rear
     ,output reg                         o_is_full
+
+
+    
     ,output [FIFO_WIDTH-1:0]            o_front
     ,output reg                         o_vld 
     ,output reg                         o_empty
+    ,output reg [FIFO_DEPTH_BIT:0]      rptr
+    ,output reg [FIFO_DEPTH_BIT:0]      wptr
 );
 
-    logic   [FIFO_DEPTH_BIT:0]          rptr, wptr, mark_ptr;
     logic   [FIFO_WIDTH-1:0]            data_in_bram;
+    logic   [FIFO_DEPTH_BIT-1:0]        mark_ptr;
+    
+    logic   [FIFO_DEPTH_BIT:0]          sub_wptr;
+    logic   [FIFO_DEPTH_BIT:0]          sub_rptr;
     
     logic                               fbit_comp, pointer_equal;    
-    logic                               push;
-                        
-    logic                               we_counter, re, we_bram;
-    logic                               almost_full, almost_empty;
-    logic   [FIFO_DEPTH_BIT:0]          pointer_sub;                        
+    logic                               push, count_push;
     
-    localparam FIFO_DEPTH = 2**FIFO_DEPTH_BIT;
-    localparam FIFO_DEPTH_SUB_1 = FIFO_DEPTH - 1;
+    logic                               data_vld, almost_full, almost_empty;                      
+    logic                               we_counter, we_counter_pipe, re, re_pipe, we_bram;
+    logic                               empty_fbit_comp, full_fbit_comp;
+    logic                               empty_pointer_equal, full_pointer_equal;
     
-    assign fbit_comp = wptr[FIFO_DEPTH_BIT] ^ rptr[FIFO_DEPTH_BIT];
-    assign pointer_sub = wptr - rptr;
-    assign pointer_equal = wptr[FIFO_DEPTH_BIT - 1:0] == rptr[FIFO_DEPTH_BIT - 1:0];
+    c_addsub_0 sub_wpointer (
+      .A(wptr),      // input wire [11 : 0] A
+      .B(1),      // input wire [11 : 0] B
+      .CLK(clk),  // input wire CLK
+      .S(sub_wptr)      // output wire [11 : 0] S
+    );
     
-        
+    c_addsub_0 sub_rpointer (
+      .A(rptr),      // input wire [11 : 0] A
+      .B(1),      // input wire [11 : 0] B
+      .CLK(clk),  // input wire CLK
+      .S(sub_rptr)      // output wire [11 : 0] S
+    );
+    assign empty_fbit_comp = sub_wptr[FIFO_DEPTH_BIT] ^ rptr[FIFO_DEPTH_BIT];  
+    assign empty_pointer_equal = (sub_wptr[FIFO_DEPTH_BIT-1:0] ^ rptr[FIFO_DEPTH_BIT-1:0])   ? 0:1;   
+
+    assign full_fbit_comp = wptr[FIFO_DEPTH_BIT] ^ sub_rptr[FIFO_DEPTH_BIT];  
+    assign full_pointer_equal = (wptr[FIFO_DEPTH_BIT-1:0] ^ sub_rptr[FIFO_DEPTH_BIT-1:0]) ? 0:1; 
+    
+    assign fbit_comp = wptr[FIFO_DEPTH_BIT] ^ rptr[FIFO_DEPTH_BIT];  
+    assign pointer_equal = (wptr[FIFO_DEPTH_BIT-1:0] ^ rptr[FIFO_DEPTH_BIT-1:0]) ? 0:1; 
+    
     always @(*)
     begin
-        almost_full = pointer_sub == FIFO_DEPTH_SUB_1 | pointer_sub == FIFO_DEPTH;
-        almost_empty = pointer_sub == 1 | pointer_sub == 0;
-        o_is_full = fbit_comp & pointer_equal;
-        o_empty = (~fbit_comp) & pointer_equal;
+        almost_full = (full_fbit_comp & full_pointer_equal & we_counter_pipe) || (fbit_comp & pointer_equal);
+        almost_empty = ((~empty_fbit_comp) & empty_pointer_equal & re_pipe) || ((~fbit_comp) & pointer_equal);
+        o_is_full = (fbit_comp & pointer_equal);
+        o_empty = ((~fbit_comp) & pointer_equal);
     end
     
     assign re = (~almost_empty) & i_pop;
     assign we_counter = (~almost_full) & i_push;
-    assign we_bram = (~o_is_full) & i_push;
+    assign we_bram = we_counter | push;
+
     
     blk_mem_gen_0 b(
         .clka       (clk),
@@ -58,8 +82,6 @@ module sample_fifo #(
         .doutb      (o_front),
         .wea        (we_bram)
     );
-    
-
     
     // READ_COUNTER
     counter_with_lat #(.WIDTH(FIFO_DEPTH_BIT+1)) count_rptr (
@@ -84,13 +106,44 @@ module sample_fifo #(
     );
     
     pipeline #(.STAGES(3), .DWIDTH(1), .RST_VAL(1'('d0)))
-    vld_pipe_inst0
-    (
+        vld_pipe_inst0
+        (
         .clk(clk), 
         .rst_n(rst_n), 
         .in_data(re), 
         .out_data_lst(o_vld)
-    );
+        );
+        
+    pipeline #(.STAGES(1), .DWIDTH(1), .RST_VAL(1'('d0)))
+    we_counter_pipe_inst0
+    (
+    .clk(clk), 
+    .rst_n(rst_n), 
+    .in_data(we_counter), 
+    .out_data_lst(we_counter_pipe)
+    );   
+    
+    pipeline #(.STAGES(1), .DWIDTH(1), .RST_VAL(1'('d0)))
+    re_pipe_inst0
+    (
+    .clk(clk), 
+    .rst_n(rst_n), 
+    .in_data(re), 
+    .out_data_lst(re_pipe)
+    );     
+
+    
+    always @(posedge clk)
+    begin
+        if (!rst_n)
+        begin
+            push <= 0;
+        end
+        else
+        begin
+            push <= i_push;
+        end
+    end
     
     pipeline #(.STAGES(1), .DWIDTH(FIFO_WIDTH), .RST_VAL(1'('d0)))
     data_pipe_inst0
@@ -98,14 +151,6 @@ module sample_fifo #(
         .clk(clk), .rst_n(rst_n), 
         .in_data(i_rear), 
         .out_data_lst(data_in_bram)
-    );
-    
-    pipeline #(.STAGES(1), .DWIDTH(1), .RST_VAL(1'('d0)))
-    push_pipe_inst0
-    (
-        .clk(clk), .rst_n(rst_n), 
-        .in_data(i_push),
-        .out_data_lst(push)
     );
     
     //mark
